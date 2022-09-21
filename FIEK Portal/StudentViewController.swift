5f27a90879792a468dd7bf62ed3db9580a95e2a5
Fisnik <fisnik@Fisniks-Mac.local>
2022-09-21 04:55:29 +0200

    Added Student view, where student attendance is showed in a table view

---
+++ FIEK Portal/StudentViewController.swift
@@ -0,0 +1,39 @@
+//
+//  StudentViewController.swift
+//  FIEK Portal
+//
+//  Created by Fisnik on 21/09/2022.
+//
+
+import UIKit
+
+class StudentViewController: UIViewController, UITableViewDataSource,  UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "studentCellId", for: indexPath)
+        
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
